d0f9a03033cfa618babd50a2d638e8de01062265
NT60502 <mansub.han@nts-corp.com>
2020-02-20 14:00:52 +0900

    add spritesmith

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const spritesmith = require("gulp.spritesmith");
 
 gulp.task("sass", function() {
   return gulp
@@ -12,4 +13,24 @@ gulp.task("sass", function() {
       })
     )
     .pipe(gulp.dest("css/"));
+});
+
+gulp.task("sprite", function() {
+  const spriteData = gulp.src("sprites/*.png").pipe(
+    spritesmith({
+      imgName: "sprite.png",
+      cssName: "_sprite.scss",
+      imgPath: "../img/sprite.png"
+    })
+  );
+
+  const imgStream = new Promise(function(resolve) {
+    spriteData.img.pipe(gulp.dest("img/")).on("end", resolve);
+  });
+
+  const cssStream = new Promise(function(resolve) {
+    spriteData.css.pipe(gulp.dest("scss/")).on("end", resolve);
+  });
+
+  return Promise.all([imgStream, cssStream]);
 });
