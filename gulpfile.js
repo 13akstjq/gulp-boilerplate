7e2cf5674cf04be8bab904237174eae3ec0203a6
NT60502 <mansub.han@nts-corp.com>
2020-02-20 11:56:32 +0900

    add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function() {
   return gulp
     .src("scss/*.scss")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
     .pipe(gulp.dest("css/"));
 });
